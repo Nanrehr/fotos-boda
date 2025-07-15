dad3a08999693ccba6cfe2fb83a676d3f9471cf6
Nanrehr <134048682+Nanrehr@users.noreply.github.com>
2025-07-15 18:19:51 +0200

    Amazon s3 conectado

--- app.py
+++ app.py
@@ -1,20 +1,33 @@
+from flask import Flask, render_template, request, redirect
+import boto3
 import os
-from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
-UPLOAD_FOLDER = 'uploads'
-os.makedirs(UPLOAD_FOLDER, exist_ok=True)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route('/', methods=['GET', 'POST'])
-def upload():
-    if request.method == 'POST':
-        f = request.files.get('file')
-        if f and f.filename:
-            f.save(os.path.join(app.config['UPLOAD_FOLDER'], f.filename))
-        return '', 204  # Dropzone espera una respuesta vacía
+BUCKET_NAME = 'fotos-boda-mar-jc'
+s3 = boto3.client('s3')
+
+@app.route('/')
+def home():
     return render_template('upload.html')
 
+@app.route('/upload', methods=['POST'])
+def upload():
+    files = request.files.getlist('fotos')
+    for f in files:
+        filename = secure_filename(f.filename)
+        s3.upload_fileobj(f, BUCKET_NAME, filename, ExtraArgs={'ACL': 'public-read'})
+    return redirect('/gallery')
+
+@app.route('/gallery')
+def gallery():
+    response = s3.list_objects_v2(Bucket=BUCKET_NAME)
+    fotos = []
+    if 'Contents' in response:
+        for obj in response['Contents']:
+            fotos.append(f"https://{BUCKET_NAME}.s3.amazonaws.com/{obj['Key']}")
+    return render_template('gallery.html', fotos=fotos)
+
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True)
